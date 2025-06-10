3f1d37c09f4d9f81a3eb52751ac3252c486668dc
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-06-10 19:32:54 -0400

    Update Run_fastp.sh

--- FastQC/Run_fastp.sh
+++ FastQC/Run_fastp.sh
@@ -1,3 +1,5 @@
+# Reference: https://github.com/OpenGene/fastp
+
 fastp -i /path/to/R1.fastq.gz -I /path/to/R2.fastq.gz \
 -o /path/to/R1_fastp.fastq.gz -O /path/to/R2_fastp.fastq.gz \
 -h /path/to/R12_fastp.html -j /path/to/R12_fastp.json
